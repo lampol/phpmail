d8c3de8c9f10b48e9f28cca4166f203bf6b4fdab
root <root@lampol.localdomain>
2017-10-31 22:53:29 +0800

    phpmail init

---
+++ src/Mail.php
@@ -0,0 +1,69 @@
+<?php
+//=======================================================
+//                Hello World
+//
+//
+//
+//======================================================
+
+namespace lampol;
+use lampol\Exception;
+
+
+class Mail extends PHPMailer{
+	
+	protected $errMsg='';
+
+
+	//初始化发送的配置
+	public function __construct($options,$exception){
+		if(!array_key_exists('emailName',$options)||!array_key_exists('emailPass',$options)||!array_key_exists('host',$options)){
+			$res = ['status'=>'fail','info'=>'必须传入参数emailName,emailPass,host'];
+			$this->errMsg =  json_encode($res,JSON_UNESCAPED_UNICODE);
+		}
+		parent::__construct($exception=true);
+		isset($options['debug'])?$this->SMTPDebug=$options['debug']:0;                    
+		$this->isSMTP();                         
+		$this->Host = $options['host'];;            
+		$this->SMTPAuth = true;                  
+		$this->Username = $options['emailName']; 
+		$this->Password = $options['emailPass'];            
+		isset($options['secure'])?$this->SMTPSecure=$options['secure']:'tls';              
+		isset($options['port'])?$this->Port=$options['port']:25;                        
+		$this->setFrom($options['emailName']);      
+
+	}
+
+	//发送邮件
+	public function sendMail($to,$subject,$content,$attachPath){
+		if($this->errMsg!=''){
+			return $this->errMsg;
+		}
+		if($to==''||$subject==''||$content==''){
+			$res = ['status'=>'fail','info'=>'发送人,发送主题,发送内容不能为空'];
+			return json_encode($res,JSON_UNESCAPED_UNICODE);
+		}
+		$this->addAddress($to);
+		if(isset($attachPath)){
+			if(file_exists($attachPath)){
+				$this->addAttachment($attachPath);
+			}else{
+				$res = ['status'=>'fail','info'=>'附件不存在'];
+				return json_encode($res,JSON_UNESCAPED_UNICODE);
+			}
+		}
+		$this->isHTML(true);                         
+		$this->Subject = $subject;              
+		$this->Body    = $content;
+		try{
+			$this->send();		
+			$res = ['status'=>'success','info'=>'发送成功'];
+		}catch (Exception $e) {
+			$res = ['status'=>'fail','info'=>$this->ErrorInfo];
+		}
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	
+	}
+
+
+} 
---
+++ test.php
@@ -0,0 +1,31 @@
+<?php
+use lampol\Mail;
+
+require 'vendor/autoload.php';
+
+//现在只支持SMTP
+
+
+//debug  0 关闭  2开启 默认关闭
+//secure  开启加密  ssl  和 tls 两个 默认 tls
+//port   端口  默认 25
+//emailName  email 地址    必传
+//emailPass  email 密码    必传
+//host       email 服务器  必传
+
+$config = ['emailName'=>'yuanyierban@163.com','emailPass'=>'qwer1234','host'=>'smtp.163.com'];
+
+//$to 收件人邮箱     必选
+//$subject 邮件主题  必选
+//$content 邮件内容  必选
+//$attachPath 附件路径  /tmp/file.php 可选
+//sendMail($to,$subject,$content)
+//
+//成功返回数据
+//{"status":"success","info":"发送成功"}
+
+$mail = new Mail($config,true);
+$res = $mail->sendMail('807968192@qq.com','Ceshi','wojiushiceshiyixia','/tmp/hello.doc');
+echo $res;
+
+
